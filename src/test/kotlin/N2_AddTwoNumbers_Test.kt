bdbf2e89a9eaceb331ec4c130da8990cc21a7394
i.chugreev <kuroko.kpytokc@yandex.ru>
2024-10-07 18:08:52 +0300

    Add Tests

---
+++ src/test/kotlin/N2_AddTwoNumbers_Test.kt
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test
+
+import org.assertj.core.api.Assertions.*
+
+class N2_AddTwoNumbers_Test {
+
+    @Test
+    fun addTwoNumbers_Test1() {
+        val l1 = N2_AddTwoNumbers.ListNode(intArrayOf(2,4,3))
+        val l2 = N2_AddTwoNumbers.ListNode(intArrayOf(5,6,4))
+        assertThat(N2_AddTwoNumbers().addTwoNumbers(l1, l2)!!.contentToString()).isEqualTo("[7, 0, 8]")
+    }
+}
