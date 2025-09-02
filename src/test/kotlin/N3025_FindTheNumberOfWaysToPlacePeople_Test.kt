ceb4fc485eab44aa9daa39f6a04d9970c5ff0488
i.chugreev <kuroko.kpytokc@yandex.ru>
2025-09-02 21:24:18 +0300

    3025 Find The Number Of Ways To Place People

---
+++ src/test/kotlin/N3025_FindTheNumberOfWaysToPlacePeople_Test.kt
@@ -0,0 +1,23 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class N3025_FindTheNumberOfWaysToPlacePeople_Test {
+
+    @Test
+    fun numberOfPairs_Test1() {
+        val points = arrayOf(intArrayOf(1, 1), intArrayOf(2, 2), intArrayOf(3, 3))
+        assertThat(N3025_FindTheNumberOfWaysToPlacePeople().numberOfPairs(points)).isEqualTo(0)
+    }
+
+    @Test
+    fun numberOfPairs_Test2() {
+        val points = arrayOf(intArrayOf(6, 2), intArrayOf(4, 4), intArrayOf(2, 6))
+        assertThat(N3025_FindTheNumberOfWaysToPlacePeople().numberOfPairs(points)).isEqualTo(2)
+    }
+
+    @Test
+    fun numberOfPairs_Test3() {
+        val points = arrayOf(intArrayOf(3, 1), intArrayOf(1, 3), intArrayOf(1, 1))
+        assertThat(N3025_FindTheNumberOfWaysToPlacePeople().numberOfPairs(points)).isEqualTo(2)
+    }
+}
