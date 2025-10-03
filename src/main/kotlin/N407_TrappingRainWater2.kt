ee92717a8bf378fa8b28f28e92a0c21dd5489fd2
i.chugreev <kuroko.kpytokc@yandex.ru>
2025-10-03 21:00:03 +0300

    refactor 407 Trapping Rain Water 2

--- src/main/kotlin/N407_TrappingRainWater2.kt
+++ src/main/kotlin/N407_TrappingRainWater2.kt
@@ -1,41 +1,48 @@
-import java.util.PriorityQueue
+import utils.PriorityQueueWithBulkInitBuilder
 
 class N407_TrappingRainWater2 {
 
-    private class Cell(var height: Int, var row: Int, var col: Int) : Comparable<Cell?> {
-        override fun compareTo(other: Cell?) = this.height.compareTo(other!!.height)
-    }
+    private data class Cell(var height: Int, var row: Int, var col: Int)
 
     private val dirs = arrayOf(intArrayOf(0, 1), intArrayOf(0, -1), intArrayOf(1, 0), intArrayOf(-1, 0))
 
     fun trapRainWater(heightMap: Array<IntArray>): Int {
-        val visited = Array(heightMap.size) { BooleanArray(heightMap[0].size) }
-        val boundary = PriorityQueue<Cell>()
-        heightMap.indices.forEach {
-            boundary.offer(Cell(heightMap[it][0], it, 0))
-            boundary.offer(Cell(heightMap[it][heightMap[0].size - 1], it, heightMap[0].size - 1))
-            visited[it][0] = true
-            visited[it][heightMap[0].size - 1] = true
+        val n = heightMap.size
+        val m = heightMap[0].size
+        if (n == 1 || m == 1) return 0
+        val nMinusOne = n - 1
+        val mMinusOne = m - 1
+        val visited = Array(n) { BooleanArray(m) }
+        val pqInitData = ArrayList<Cell>((nMinusOne shl 1) + (mMinusOne shl 1))
+        for (i in 0..nMinusOne) {
+            pqInitData += Cell(heightMap[i][0], i, 0)
+            pqInitData += Cell(heightMap[i][mMinusOne], i, mMinusOne)
+            visited[i][0] = true
+            visited[i][mMinusOne] = true
         }
-        heightMap[0].indices.forEach {
-            boundary.offer(Cell(heightMap[0][it], 0, it))
-            boundary.offer(Cell(heightMap[heightMap.size - 1][it], heightMap.size - 1, it))
-            visited[0][it] = true
-            visited[heightMap.size - 1][it] = true
+        for (i in 1..<mMinusOne) {
+            pqInitData += Cell(heightMap[0][i], 0, i)
+            pqInitData += Cell(heightMap[nMinusOne][i], nMinusOne, i)
+            visited[0][i] = true
+            visited[nMinusOne][i] = true
         }
+        val boundary = PriorityQueueWithBulkInitBuilder
+            .buildPriorityQueueWithBulkInit(pqInitData, compareBy { it.height })
         var totalWaterVolume = 0
         while (boundary.isNotEmpty()) {
             val currentCell = boundary.poll()
-            dirs.forEach {
-                val neighborRow = currentCell.row + it[0]
-                val neighborCol = currentCell.col + it[1]
+            for (dir in dirs) {
+                val neighborRow = currentCell.row + dir[0]
+                val neighborCol = currentCell.col + dir[1]
                 if (
-                    neighborRow >= 0 && neighborCol >= 0 && neighborRow < heightMap.size
-                    && neighborCol < heightMap[0].size && !visited[neighborRow][neighborCol]
+                    neighborRow > -1 && neighborCol > -1 && neighborRow < n && neighborCol < m
+                    && !visited[neighborRow][neighborCol]
                 ) {
                     val neighborHeight = heightMap[neighborRow][neighborCol]
-                    if (neighborHeight < currentCell.height) totalWaterVolume += currentCell.height - neighborHeight
-                    boundary.offer(Cell(maxOf(neighborHeight, currentCell.height), neighborRow, neighborCol))
+                    if (neighborHeight < currentCell.height) {
+                        totalWaterVolume += currentCell.height - neighborHeight
+                    }
+                    boundary.add(Cell(maxOf(neighborHeight, currentCell.height), neighborRow, neighborCol))
                     visited[neighborRow][neighborCol] = true
                 }
             }
