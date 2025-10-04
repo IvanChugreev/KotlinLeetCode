59b9766f4689b105f4158276cdd60682c5ac2802
i.chugreev <kuroko.kpytokc@yandex.ru>
2025-10-04 10:32:53 +0300

    refactor 11 Container With Most Water

--- src/main/kotlin/N11_ContainerWithMostWater.kt
+++ src/main/kotlin/N11_ContainerWithMostWater.kt
@@ -3,15 +3,14 @@ class N11_ContainerWithMostWater {
         var left = 0
         var right = height.lastIndex
         var maxArea = 0
-        var lastMax: Int
         while (left < right) {
-            maxArea = maxOf(maxArea, minOf(height[left], height[right]) * (right - left))
-            if (height[left] < height[right]) {
-                lastMax = height[left++]
-                while (left < height.size && lastMax >= height[left]) ++left
-            } else {
-                lastMax = height[right--]
-                while (right > -1 && lastMax >= height[right]) --right
+            val minHeight = minOf(height[left], height[right])
+            maxArea = maxOf(maxArea, minHeight * (right - left))
+            while (height[left] <= minHeight && left < right) {
+                ++left
+            }
+            while (height[right] <= minHeight && left < right) {
+                --right
             }
         }
         return maxArea
