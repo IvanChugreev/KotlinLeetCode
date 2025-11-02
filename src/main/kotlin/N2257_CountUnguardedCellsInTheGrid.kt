3085aefc0d41c8f9f7c398c1cce6f65b8155b493
i.chugreev <kuroko.kpytokc@yandex.ru>
2025-11-02 10:50:20 +0300

    refactor 2257 Count Unguarded Cells In The Grid

--- src/main/kotlin/N2257_CountUnguardedCellsInTheGrid.kt
+++ src/main/kotlin/N2257_CountUnguardedCellsInTheGrid.kt
@@ -1,57 +1,38 @@
 class N2257_CountUnguardedCellsInTheGrid {
-    enum class TypeCell {
-        UNGUARDED,
-        GUARDED_HORIZONTAL,
-        GUARDED_VERTICAL,
-        GUARDED_BOTH,
-        WALL
-    }
-
     fun countUnguarded(m: Int, n: Int, guards: Array<IntArray>, walls: Array<IntArray>): Int {
-        val grid = Array(m) { Array(n) { TypeCell.UNGUARDED } }
-        fun updateCells(row: Int, col: Int) {
-            if (grid[row][col] == TypeCell.GUARDED_BOTH) return
-            var ind: Int
-            if (grid[row][col] != TypeCell.GUARDED_HORIZONTAL) {
-                ind = col
-                while (--ind > -1 && grid[row][ind] != TypeCell.WALL) {
-                    if (grid[row][ind] == TypeCell.GUARDED_VERTICAL) {
-                        grid[row][ind] = TypeCell.GUARDED_BOTH
-                    } else {
-                        grid[row][ind] = TypeCell.GUARDED_HORIZONTAL
-                    }
-                }
-                ind = col
-                while (++ind < n && grid[row][ind] != TypeCell.WALL) {
-                    if (grid[row][ind] == TypeCell.GUARDED_VERTICAL) {
-                        grid[row][ind] = TypeCell.GUARDED_BOTH
-                    } else {
-                        grid[row][ind] = TypeCell.GUARDED_HORIZONTAL
-                    }
-                }
-            }
-            if (grid[row][col] != TypeCell.GUARDED_VERTICAL) {
-                ind = row
-                while (--ind > -1 && grid[ind][col] != TypeCell.WALL) {
-                    if (grid[ind][col] == TypeCell.GUARDED_HORIZONTAL) {
-                        grid[ind][col] = TypeCell.GUARDED_BOTH
-                    } else {
-                        grid[ind][col] = TypeCell.GUARDED_VERTICAL
-                    }
-                }
-                ind = row
-                while (++ind < m && grid[ind][col] != TypeCell.WALL) {
-                    if (grid[ind][col] == TypeCell.GUARDED_HORIZONTAL) {
-                        grid[ind][col] = TypeCell.GUARDED_BOTH
-                    } else {
-                        grid[ind][col] = TypeCell.GUARDED_VERTICAL
-                    }
-                }
-            }
-            grid[row][col] = TypeCell.GUARDED_BOTH
+        val grid = Array(m) { IntArray(n) }
+        for ((y, x) in walls) {
+            grid[y][x] = 2
         }
-        walls.forEach { grid[it[0]][it[1]] = TypeCell.WALL }
-        guards.forEach { updateCells(it[0], it[1]) }
-        return grid.sumOf { it.count { cell -> cell == TypeCell.UNGUARDED } }
+        for ((y, x) in guards) {
+            grid[y][x] = 3
+        }
+        for ((y, x) in guards) {
+            var i = y + 1
+            while (i < m && grid[i][x] < 2) {
+                grid[i++][x] = 1
+            }
+            i = y - 1
+            while (i >= 0 && grid[i][x] < 2) {
+                grid[i--][x] = 1
+            }
+            i = x + 1
+            while (i < n && grid[y][i] < 2) {
+                grid[y][i++] = 1
+            }
+            i = x - 1
+            while (i >= 0 && grid[y][i] < 2) {
+                grid[y][i--] = 1
+            }
+        }
+        var result = 0
+        for (row in grid) {
+            for (value in row) {
+                if (value < 1) {
+                    ++result
+                }
+            }
+        }
+        return result
     }
 }
