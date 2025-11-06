92815bdb2a7c259bc1492d8ff0c2b6a3f0710dab
i.chugreev <kuroko.kpytokc@yandex.ru>
2025-11-07 00:01:33 +0300

    add UnionFind

--- src/main/kotlin/N778_SwimInRisingWater.kt
+++ src/main/kotlin/N778_SwimInRisingWater.kt
@@ -1,3 +1,5 @@
+import utils.UnionFind
+
 class N778_SwimInRisingWater {
 
     fun swimInWater(grid: Array<IntArray>): Int {
@@ -28,33 +30,5 @@ class N778_SwimInRisingWater {
             }
         }
         return currentHeight
-    }
-
-    private class UnionFind(n: Int) {
-        private val parent = IntArray(n) { it }
-        private val rank = IntArray(n)
-
-        fun find(x: Int): Int {
-            var root = x
-            while (parent[root] != root) {
-                root = parent[root]
-            }
-            var currParent = x
-            while (parent[currParent] != currParent) {
-                parent[currParent] = root.also { currParent = parent[currParent] }
-            }
-            return root
-        }
-
-        fun union(x: Int, y: Int) {
-            val xRoot = find(x)
-            val yRoot = find(y)
-            if (xRoot == yRoot) return
-            when {
-                rank[xRoot] < rank[yRoot] -> parent[xRoot] = yRoot
-                rank[xRoot] > rank[yRoot] -> parent[yRoot] = xRoot
-                else -> parent[yRoot] = xRoot.also { ++rank[xRoot] }
-            }
-        }
     }
 }
