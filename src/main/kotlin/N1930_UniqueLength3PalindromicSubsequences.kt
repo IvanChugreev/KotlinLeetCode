5e300c7fda45152b570285e2deebc3cf262ea7be
i.chugreev <kuroko.kpytokc@yandex.ru>
2025-11-21 19:55:33 +0300

    refactor 1930 Unique Length-3 Palindromic Subsequences

--- src/main/kotlin/N1930_UniqueLength3PalindromicSubsequences.kt
+++ src/main/kotlin/N1930_UniqueLength3PalindromicSubsequences.kt
@@ -1,22 +1,28 @@
 class N1930_UniqueLength3PalindromicSubsequences {
     fun countPalindromicSubsequence(s: String): Int {
-        val last = IntArray(26)
-        s.forEachIndexed { ind, symbol -> last[symbol - 'a'] = ind }
-        var count = 0
-        (0..s.length - 3).forEach { ind ->
-            val curr = s[ind] - 'a'
-            if (last[curr] != ind && last[curr] != ind + 1 && last[curr] != 0) {
-                val between = BooleanArray(26)
-                (ind + 1..<last[curr]).forEach {
-                    val currCode = s[it] - 'a'
-                    if (!between[currCode]) {
-                        between[currCode] = true
-                        ++count
-                    }
+        val first = IntArray(26) { -1 }
+        val last = IntArray(26) { -1 }
+        for (i in s.indices) {
+            val charIndex = s[i] - 'a'
+            if (first[charIndex] == -1) {
+                first[charIndex] = i
+            }
+            last[charIndex] = i
+        }
+        var res = 0
+        for (i in 0..25) {
+            val firstIndex = first[i]
+            val lastIndex = last[i]
+            if (firstIndex == -1 || lastIndex - firstIndex < 2) continue
+            val uniqueChars = BooleanArray(26)
+            for (j in firstIndex + 1 until lastIndex) {
+                val charIndex = s[j] - 'a'
+                if (!uniqueChars[charIndex]) {
+                    uniqueChars[charIndex] = true
+                    ++res
                 }
-                last[curr] = 0
             }
         }
-        return count
+        return res
     }
 }
